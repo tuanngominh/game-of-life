699780adce9524a6b81dbb5a5caa0e5ddfc88033
Tuan Ngo <tuan@trexanhlab.com>
2017-01-11 11:11:58 +0700

    revise game description content

--- src/components/Game.js
+++ src/components/Game.js
@@ -167,15 +167,14 @@ class Game extends Component {
           <div className='col-xs-12'>
             <hr/>
             <p>
-              Implementation of famous <a href="https://www.youtube.com/watch?v=E8kUJL04ELA" target="_blank">Game Of Life</a> which is invented by John Conway. 
-              Reference from <a href="https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life" target="_blank">wikipedia</a>
+              Implementation of famous <a href="https://www.youtube.com/watch?v=E8kUJL04ELA" target="_blank">Game Of Life</a> which is invented by John Conway.
             </p>
             <p>
               Check full code on <a href="https://github.com/tuanngominh/game-of-life" target="_blank">github</a>. The game is written on <a href="https://facebook.github.io/react/" target="_blank">react</a>.
             </p>            
           </div>
           <div className='col-sm-6 col-sm-offset-3'>
-            <img className="img-responsive" src="//i.ytimg.com/vi/E8kUJL04ELA/maxresdefault.jpg"/>
+            <img className="img-responsive" src="//i.ytimg.com/vi/E8kUJL04ELA/maxresdefault.jpg" alt="John Conway portrait"/>
           </div>
         </div>
       </div>
